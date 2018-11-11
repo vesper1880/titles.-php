e72c13a07170b169b0a90bca1a16afcbb6c27775
vesper1880 <41402645+vesper1880@users.noreply.github.com>
2018-11-11 20:20:08 +0000

    Create Titlesm.php

---
+++ Titlesm.php
@@ -0,0 +1,25 @@
+<?php 
+$name = str_replace("/","",$_SERVER['REQUEST_URI']);
+$var = rand(1,20);
+if($var == 1){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 2){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 3){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 4){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 5){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 6){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 7){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 8){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 9){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 10){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 11){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 12){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 13){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 14){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 15){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 16){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 17){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 18){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 19){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+if($var == 20){echo "$name Here Is Your kid. ✋👇 Liked it? Click Here for More".PHP_EOL;}
+
+?>
